6dd60450f3bb625bc5c2e5318bc08ea627e1152b
SKANDA MAYYA <153281469+skmayya1@users.noreply.github.com>
2024-10-22 19:47:59 +0530

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    host: '0.0.0.0', // Allow external access
+    port: process.env.PORT || 4173, // Use the port Render provides or default to 4173
+  }
 })
